dc7a8368a92c0e67bf79e29771af6f63ce0c41cb
lewhfree <48800753+lewhfree@users.noreply.github.com>
2026-03-24 22:03:54 -0600

    Cleanup

--- main.py
+++ main.py
@@ -1,57 +1,56 @@
-from future.moves.sys import set_coroutine_origin_tracking_depth
 import librosa
-import math
 import ffmpeg
 import numpy as np
-import matplotlib
-import scipy
-import xxhash
+import glob
 from scipy import ndimage
+import os
 
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-filename: str = "audio.flac"
+def genDataPairs(filename, searchSize, cutoff):
+    plotSearchSize = searchSize
+    DBCutoffVal = cutoff
 
-stream=ffmpeg.input(filename)
-stream=ffmpeg.output(stream, "audio.out.mp3", ar=11025, ac=1)
-stream.run(overwrite_output=True, quiet=True)
+    print(filename)
+    stream=ffmpeg.input(filename)
+    stream=ffmpeg.output(stream, "audio.tmp.mp3", ar=11025, ac=1)
+    stream.run(overwrite_output=True, quiet=True)
+    stream=ffmpeg.input("audio.tmp.mp3")
+    stream=ffmpeg.output(stream, "audio.out.mp3", ar=8000, ac=1)
+    stream.run(overwrite_output=True, quiet=True)
+    
+    audio, sr = librosa.load("audio.out.mp3", mono=True, sr=8000)
+    
+    stft = librosa.stft(y=audio)
+    stft = np.abs(stft)
+    stft = librosa.amplitude_to_db(stft, ref=np.max)
+    
+    betterPlot = ndimage.maximum_filter(stft, size=plotSearchSize)
+    mask = (stft == betterPlot) & (stft > DBCutoffVal)
+    ret = np.full_like(stft, -80.0)
+    ret[mask] = stft[mask] 
+    
+    f_indices, t_indices = np.where(mask)
+    result = list(zip(f_indices, t_indices))
+    print(f"Total peaks found: {len(result)}")
+    pps=len(result) // librosa.get_duration(y=audio, sr=sr)
+    print("pps: ", pps)
+    plain_int_tuples = [(int(x), int(y)) for x, y in result]
+    print(plain_int_tuples)
+    
+    sorted_result = sorted(plain_int_tuples, key=lambda x: x[1])
 
-audio, sr = librosa.load("audio.out.mp3", mono=True, sr=11025)
+    os.remove("audio.tmp.mp3")
+    os.remove("audio.out.mp3")
+    
+    return pps, len(result)
 
-stft = librosa.stft(y=audio)
-stft = np.abs(stft)
-stft = librosa.amplitude_to_db(stft, ref=np.max)
-# print(stft)
-# print(stft.shape)
+files = glob.glob("music/*")
 
-hash = xxhash.xxh64("test").hexdigest()
-print(hash)
-betterPlot = ndimage.maximum_filter(stft, size=8)
-mask = (stft == betterPlot) & (stft > -70)
-ret = np.full_like(stft, -80.0)  # start with -80 everywhere
-ret[mask] = stft[mask] 
-# librosa.display.specshow(ret, sr=sr, x_axis='time', y_axis='hz')
-# plt.ylim(0, 5100)
-# plt.colorbar(format='%+2.0f dB')
-# plt.title('STFT Magnitude (dB)')
-# plt.xlabel("Time (s)")
-# plt.ylabel("Frequency (Hz)")
-# plt.tight_layout()
-# plt.show()
+ppss = []
+totals = []
+for file in files:
+    pps, total = genDataPairs(file, 15, -38)
+    ppss.append(pps)
+    totals.append(total)
 
-#constelation.
-#
-result = []
-num_freqs, num_times = ret.shape
-
-for t in range(num_times):
-    for f in range(num_freqs):
-        if ret[f, t] >= -70:  # Ignore values that are -80 (silence)
-            result.append((f, t))  # Append a tuple (frequency, time)
-
-# print(result)
-print(len(result))
-
-sorted_result = sorted(result, key=lambda x: x[1])
-
-# print(sorted_result)
+print("mean pps:", np.mean(ppss))
+print("mean totals:", np.mean(totals))

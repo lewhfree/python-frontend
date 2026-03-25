ffe3d52711cb493db6f8955860641e872f95f380
lewhfree <48800753+lewhfree@users.noreply.github.com>
2026-03-25 00:54:20 -0600

    audio processing done need to send to server next

--- main.py
+++ main.py
@@ -4,49 +4,69 @@ import numpy as np
 import glob
 from scipy import ndimage
 import os
+import struct
+import xxhash
 
-def genDataPairs(filename, searchSize, cutoff):
-    plotSearchSize = searchSize
-    DBCutoffVal = cutoff
+NUM_O_FANS = 3
+N_FFT=2048
+HOP_LENGTH=N_FFT//4
+TIME_WINDOW=3
+FINAL_SAMPLE_RATE=8000
+INBETWEEN_SAMPLE_RATE=11025
+DB_CUTOFF_VAL=-38
+PLOT_SEARCH_SIZE=15
+FRAME_WIDTH_T=HOP_LENGTH/FINAL_SAMPLE_RATE
+
+def genDataPairs(filename):
 
     stream=ffmpeg.input(filename)
-    stream=ffmpeg.output(stream, "audio.tmp.mp3", ar=11025, ac=1)
+    stream=ffmpeg.output(stream, "audio.tmp.mp3", ar=INBETWEEN_SAMPLE_RATE, ac=1)
     stream.run(overwrite_output=True, quiet=True)
     stream=ffmpeg.input("audio.tmp.mp3")
-    stream=ffmpeg.output(stream, "audio.out.mp3", ar=8000, ac=1)
+    stream=ffmpeg.output(stream, "audio.out.mp3", ar=FINAL_SAMPLE_RATE, ac=1)
     stream.run(overwrite_output=True, quiet=True)
     
-    audio, sr = librosa.load("audio.out.mp3", mono=True, sr=8000)
+    audio, sr = librosa.load("audio.out.mp3", mono=True, sr=FINAL_SAMPLE_RATE)
     
-    stft = librosa.stft(y=audio)
+    stft = librosa.stft(y=audio, hop_length=HOP_LENGTH, n_fft=N_FFT)
     stft = np.abs(stft)
     stft = librosa.amplitude_to_db(stft, ref=np.max)
     
-    betterPlot = ndimage.maximum_filter(stft, size=plotSearchSize)
-    mask = (stft == betterPlot) & (stft > DBCutoffVal)
+    betterPlot = ndimage.maximum_filter(stft, size=PLOT_SEARCH_SIZE)
+    mask = (stft == betterPlot) & (stft > DB_CUTOFF_VAL)
     ret = np.full_like(stft, -80.0)
     ret[mask] = stft[mask] 
    
     f_indices, t_indices = np.where(mask)
-    time_in_seconds = librosa.frames_to_time(t_indices, sr=sr)
-    freq_map = librosa.fft_frequencies(sr=sr, n_fft=2048)
-    freq_in_hz = freq_map[f_indices]
-    result = list(zip(freq_in_hz, time_in_seconds))
-    pps = len(result) // librosa.get_duration(y=audio, sr=sr)
-    formatted_tuples = [(round(float(f), 2), round(float(t), 2)) for f, t in result]
-    sorted_result = sorted(formatted_tuples, key=lambda x: x[0])
-    print(sorted_result)
+    result = list(zip(t_indices, f_indices))
+    formatted_tuples = [(int(f), int(t)) for f, t in result]
+    sorted_result = sorted(formatted_tuples, key=lambda x: (x[0], x[1]))#list is touples of time, freq
 
     os.remove("audio.tmp.mp3")
     os.remove("audio.out.mp3")
     
-    return pps, len(result)
+    return sorted_result
 
 files = glob.glob("music/*")
-
-ppss = []
-totals = []
+files = ["music/audio.flac"]
+hashes = []
 for file in files:
-    pps, total = genDataPairs(file, 15, -38)
-    ppss.append(pps)
-    totals.append(total)
+    results = genDataPairs(file)
+    for i in range(len(results)):
+        origin = results[i]
+        window_min = TIME_WINDOW
+        fans_found = 0
+        for j in range(1,100 if (100+i)<len(results) else 0):
+            if ((origin[0] + window_min) <= results[i+j][0]) and fans_found < NUM_O_FANS:
+                window_min += 1
+                fans_found += 1 
+                point = results[i+j]
+                delta_t = point[0] - origin[0]
+                origin_freq = origin[1]
+                end_freq = point[1]
+                packed_bytes = struct.pack("<III", delta_t, origin_freq, end_freq)
+                hash = xxhash.xxh64(packed_bytes).intdigest()
+                t=FRAME_WIDTH_T*origin[0]*1000
+                hashes.append((hash, t))
+
+#send the hashes to the server for ingestion
